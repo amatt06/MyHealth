ce36d7a215c928f2dbeb8824d99dbac308c3b172
Adam Matthews <62410149+amatt06@users.noreply.github.com>
2023-05-13 15:15:03 +1000

    rename and test

--- src/main/java/rmit/myhealth/MainWindowController.java
+++ src/main/java/rmit/myhealth/MainWindowController.java
@@ -3,7 +3,7 @@ package rmit.myhealth;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MainWindowController {
     @FXML
     private Label welcomeText;
 
