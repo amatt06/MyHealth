f771c654a45a69f572252a5bcd3e6e0c57556253
Adam Matthews <62410149+amatt06@users.noreply.github.com>
2023-05-13 17:44:42 +1000

    login page layout

--- src/main/java/rmit/myhealth/MainWindowController.java
+++ src/main/java/rmit/myhealth/MainWindowController.java
@@ -1,14 +1,42 @@
 package rmit.myhealth;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class MainWindowController {
     @FXML
-    private Label welcomeText;
+    private Label titleLabel;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Button createUserButton;
+
+    @FXML
+    protected void initialize() {
+        titleLabel.setText("MyHealth");
+    }
+
+    @FXML
+    protected void login() {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        // Perform login logic here
+    }
+
+    @FXML
+    protected void createUser() {
+        // Show the create user form or navigate to the create user page
     }
 }
--- src/main/java/rmit/myhealth/MyHealthApp.java
+++ src/main/java/rmit/myhealth/MyHealthApp.java
@@ -11,8 +11,8 @@ public class MyHealthApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MyHealthApp.class.getResource("MainWindow.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 600, 680);
+        stage.setTitle("Welcome to MyHealth");
         stage.setScene(scene);
         stage.show();
     }
