a7f9945f8ae40dc920c180ac564acfc1259296fd
Adam Matthews <62410149+amatt06@users.noreply.github.com>
2023-05-28 21:36:36 +1000

    new record added

--- src/main/java/rmit/myhealth/NewRecordController.java
+++ src/main/java/rmit/myhealth/NewRecordController.java
@@ -1,16 +1,63 @@
 package rmit.myhealth;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import rmit.myhealth.model.HealthRecord;
+import rmit.myhealth.model.MyHealth;
+
+import java.time.LocalDateTime;
 
 public class NewRecordController {
+    @FXML
+    private TextField weightField;
+
+    @FXML
+    private TextField temperatureField;
+
+    @FXML
+    private TextField bloodPressureUpperField;
+
+    @FXML
+    private TextField bloodPressureLowerField;
+
+    @FXML
+    private TextArea noteField;
+
+    @FXML
+    private Button cancelButton;
 
     @FXML
     private void saveRecord() {
+        if (MyHealth.getInstance().getCurrentUser() != null && MyHealth.getInstance().getCurrentUser().getHealthRecordController() != null) {
+            // Retrieve the entered information from the form
+            double weight = Double.parseDouble(weightField.getText());
+            double temperature = Double.parseDouble(temperatureField.getText());
+            int bloodPressureUpper = Integer.parseInt(bloodPressureUpperField.getText());
+            int bloodPressureLower = Integer.parseInt(bloodPressureLowerField.getText());
+            String note = noteField.getText();
+            LocalDateTime dateTime = LocalDateTime.now();
 
+            // Create a new HealthRecord object
+            HealthRecord healthRecord = new HealthRecord(weight, temperature, bloodPressureUpper, bloodPressureLower, note, dateTime);
+
+            // Add the record to the user's HealthRecordController
+            MyHealth.getInstance().getCurrentUser().getHealthRecordController().addRecord(healthRecord);
+
+            // Close the window
+            closeWindow();
+        }
     }
 
+
     @FXML
-    public void closeWindow() {
+    private void closeWindow() {
+        // Get the reference to the current window's stage
+        Stage stage = (Stage) cancelButton.getScene().getWindow();
+
+        // Close the window
+        stage.close();
     }
 }
